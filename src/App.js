d6f8af0020914ad550ae6ead8a635e22600e7f65
Rusydy <rusydymuhiddin@gmail.com>
2020-06-27 16:25:10 +0700

    adding todoList

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
 import Greeting from "./components/Greeting"
+import TodoList from "./components/TodoList"
 
 function App() { 
   return (
@@ -11,6 +12,11 @@ function App() {
       <Header />
       <Greeting />
       <MainContent />
+      <div className="todo-list">
+        <TodoList />
+        <TodoList />
+        <TodoList />
+      </div>
       <Footer />
   </div>
   )
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,11 +6,11 @@ function MainContent () {
             <p className="pharagraph">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu interdum tellus. In leo diam, tincidunt quis lorem posuere, aliquam finibus est. Integer vestibulum risus quis dui gravida, eget congue lacus viverra. Proin porttitor ligula vitae lectus porta commodo. Quisque tristique lacus sed orci pellentesque, id bibendum massa faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam nibh diam, mollis vel mollis nec, sollicitudin at massa. Fusce tincidunt et libero ac convallis. Mauris ut ornare dui, eu ornare quam. Donec vel nulla eu nisi placerat gravida. Praesent pretium, quam quis porta vestibulum, ligula arcu fermentum eros, quis dictum orci neque eget quam.
             </p>
-            <ul>
+            {/* <ul>
                 <li>Bali</li>
                 <li>KL</li>
                 <li>Lombok</li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
