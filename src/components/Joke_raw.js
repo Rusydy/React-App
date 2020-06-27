0b46821b7d80649bed79c638c6495a33608e34ac
Rusydy <rusydymuhiddin@gmail.com>
2020-06-28 05:00:05 +0700

    commit

--- src/components/Joke_raw.js
+++ src/components/Joke_raw.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 function Joke(props){
-    console.log(props)
     return (
         <div>
             <h3 style={{display: !props.question && "none"}}>Question: {props.question}</h3>
