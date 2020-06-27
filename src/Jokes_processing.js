22c8adfedc8700ca618cbf1e707ee769b8283575
Rusydy <rusydymuhiddin@gmail.com>
2020-06-28 02:04:57 +0700

    adding JokesData as an API part 2

--- src/Jokes_processing.js
+++ src/Jokes_processing.js
@@ -3,7 +3,8 @@ import Joke from "./components/Joke_raw" // how to access it if it's initialize 
 import JokeData from "./JokesData"
 
 function Jokes() {
-    const JokesComponents = JokeData.map(joke => <Joke question={joke.question} punchline={joke.punchLine} />)
+
+    const JokesComponents = JokeData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
 
     return (
         <div>
--- src/components/Joke_raw.js
+++ src/components/Joke_raw.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 function Joke(props){
+    console.log(props)
     return (
         <div>
             <h3 style={{display: !props.question && "none"}}>Question: {props.question}</h3>
-            <h3 style={{color: !props.question && "#88888"}}>Answer: {props.answer}</h3>
+            <h3 style={{color: !props.question && "#88888"}}>Answer: {props.punchLine}</h3>
             <hr/>
         </div>
     )
